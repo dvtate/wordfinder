9d9274b5cb4b3c9a108c3d3a235345f51f5915f6
Dustin Van Tate Testa <toast27@gmail.com>
2019-05-04 01:48:58 -0500

    remove sketch comment

--- fuck_windows.h
+++ fuck_windows.h
@@ -1,11 +1,6 @@
 #ifndef FUCK_WINDOWS_H
 #define FUCK_WINDOWS_H
 
-/* An open leter to Micro$oft the authors of Win$hit
- * WHY THE FUCK CANT WINDOWS BE POSTIX-COMPATABLE LIKE THE REST OF
- * THE CIVILIZED WORLD? They make me implement my own function
- * just to make this cross-platform... SO SALTY... FML
- */
 #ifdef _WIN32
 
 #include <stdlib.h>
